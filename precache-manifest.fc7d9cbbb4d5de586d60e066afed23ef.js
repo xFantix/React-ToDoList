bf5e0df78da38347696b29eb466a4fe3195d219e
Kamil Jankowski <kamil.jankoowski@outlook.com>
2020-08-07 13:35:35 +0200

    Updates

---
+++ precache-manifest.fc7d9cbbb4d5de586d60e066afed23ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73266f49b74024608be5ba8d40fd550e",
+    "url": "/React-ToDoList/index.html"
+  },
+  {
+    "revision": "1f2622c69e834334a37f",
+    "url": "/React-ToDoList/static/css/main.cae581fd.chunk.css"
+  },
+  {
+    "revision": "54eaeeced3472319fa1c",
+    "url": "/React-ToDoList/static/js/2.d5af503f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDoList/static/js/2.d5af503f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f2622c69e834334a37f",
+    "url": "/React-ToDoList/static/js/main.bb239de0.chunk.js"
+  },
+  {
+    "revision": "5d1ec9cfebf05220c117",
+    "url": "/React-ToDoList/static/js/runtime-main.2eaf8776.js"
+  }
+]);
